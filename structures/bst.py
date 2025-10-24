b7650a71925dbd2a573bdb5aba252f2315907b7e
Davidjames9610 <55407319+Davidjames9610@users.noreply.github.com>
2025-10-24 19:15:18 +0000

    started delete not working yet

--- structures/bst.py
+++ structures/bst.py
@@ -50,6 +50,49 @@ class BST:
             else:
                 self._insert_node(curr_node.right, value)
 
+    def search(self, some_node, value):
+
+        if some_node is None:
+            return False
+        if some_node.value == value:
+            return True
+        elif value < some_node.value:
+            return self.search(some_node.left, value)
+        else:
+            return self.search(some_node.right, value)
+        
+    def delete(self, value):
+        # this confuses me
+        
+        if self.tree is None:
+            return False
+        
+        found_node = self.find_node(self.tree, value)
+
+        if found_node is not None:
+            return False
+        else:
+            print('found node!', found_node.value)
+            return True
+
+    def find_node(self, some_node, value):
+
+        if some_node.value is None:
+            return None
+        if some_node.value == value:
+            return some_node
+        if value < some_node.value:
+            if some_node.left is None:
+                return None
+            else:
+                return self.find_node(some_node.left, value)
+        else:
+            if some_node.right is None:
+                return None
+            else:
+                return self.find_node(some_node.right, value)
+        
+
 #%%
 
 my_bst = BST()
@@ -57,7 +100,40 @@ my_bst = BST()
 my_bst.insert(10)
 my_bst.insert(8)
 my_bst.insert(12)
+my_bst.insert(20)
+my_bst.insert(16)
 my_bst.traverse(my_bst.tree)
+my_bst.search(my_bst.tree, 8)
+
+my_bst.delete(8)
+
+"""
+                8
+        4               12
+    2       6       10          20
+1         5       8    11          23     
+
+case 1:
+    remove 4
+    find left most child for right child
+
+case 2:
+    remove 12
+        option 1: get left most child in right child
+        option 2: get right most child in left child
+
+case 3:
+    remove 6
+        has left child but no right child
+        set parant right to child.left
+        (would have to compare parant and delete value)
+
+case 4:
+    remove 20:
+        has right child no left child
+        parent right = delete.right
+"""
+
 
 
 
